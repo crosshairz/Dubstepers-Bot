4e17887f5312f72415697f369fd43af95c0ea729
foxdemon143 <foxdemon143@gmail.com>
2013-10-30 20:39:33 -0500

    Fixed no start up bug

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ toSave.settings = mubBot.settings;
 toSave.moderators = mubBot.moderators;
 toSave.ruleSkip = ruleSkip;
 
-mubBot.misc.Rules = "1.Do Not Play Troll Songs 
-mubBot.misc.Themes = "Dubstep, Remix's, and techno"
+mubBot.misc.Rules = "1.Do Not Play Troll Songs";
+mubBot.misc.Themes = "Dubstep, Remix's, and techno";
 mubBot.misc.version = "1.0.2";
 mubBot.misc.origin = "This bot was created by Dubstepers, Alexander7370, and Foxdemon143. This bot and its script are copyrighted!";
 mubBot.misc.changelog = "Added a secondary check for history";
