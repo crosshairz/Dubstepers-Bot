dc8fcbaad4ecd5e9a88a45b82217f2908f412391
foxdemon143 <foxdemon143@gmail.com>
2013-11-05 11:06:48 -0600

    Testing / commands

--- bot.js
+++ bot.js
@@ -71,6 +71,12 @@ function UserJoin(user)
 API.sendChat("@" + user.username + " Welcome to Dubstepers");
 }
 
+API.on(API.CHAT_COMMAND, Chat.Command);
+function Chat.Command(imput)
+{
+alert(imput + ' typed as chat command');
+}
+
 botMethods.skip = function(){
     setTimeout(function(){
         if(!cancel) API.moderateForceSkip();
