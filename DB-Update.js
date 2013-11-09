e503765f2e8e0a0680af5a0760521ea05029fdfb
foxdemon143 <foxdemon143@gmail.com>
2013-11-08 23:57:23 -0600

    Try To hard

--- DB-Update.js
+++ DB-Update.js
@@ -1,2 +1,33 @@
 //this is the bot script for the new last plug API
+
+var mubBot = {};
+var ruleSkip = {};
+mubBot.misc = {};
+mubBot.settings = {};
+mubBot.moderators = {};
+mubBot.filters = {};
+botMethods = {};
+mubBot.pubVars = {};
+
 API.sendChat('/me : Running Dubstepers Script Version 1.0.1');
+
+
+
+
+API.on(API.CHAT, function(data){
+        if(data.message.indexOf('!') === 0){
+            var msg = data.message, from = data.from, fromID = data.fromID;
+            var command = msg.substring(1).split(' ');
+            if(typeof command[2] != "undefined"){
+                for(var i = 2; i<command.length; i++){
+                    command[1] = command[1] + ' ' + command[i];
+                }
+            }
+            if(mubBot.misc.ready || mubBot.admins.indexOf(fromID) > -1 || API.getUser(data.fromID).permission > 1){
+                switch(command[0].toLowerCase()){
+                  
+                  case "ping"
+                    API.sendChat("TEST");
+                  break
+               }
+            };
