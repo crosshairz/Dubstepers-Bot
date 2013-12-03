b4df846c22786273d9d660319d6b450f1ccd2ad4
Alexander7370 <alexanderkcato@gmail.com>
2013-12-02 20:13:23 -0800

    Update Custom_Theme.js

--- Custom_Theme.js
+++ Custom_Theme.js
@@ -2,4 +2,4 @@ API.chatLog("You are running a custom background now. Hopefully you like it! If 
 function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
-$('head').append('<link rel="stylesheet" type="text/css" https://github.com/Alexander7370/Dubstepers-Bot/edit/master/Custom_Theme_pic.css">');
+$('head').append('<link rel="stylesheet" type="text/css" https://raw.github.com/Alexander7370/Dubstepers-Bot/master/Custom_Theme_pic.css">');
