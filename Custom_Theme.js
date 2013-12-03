cfd467be167eca4e45d677ebdacb8d33649f9944
Alexander7370 <alexanderkcato@gmail.com>
2013-12-02 19:50:13 -0800

    Update Custom_Theme.js

--- Custom_Theme.js
+++ Custom_Theme.js
@@ -1,5 +1,5 @@
 API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
 function initEnvironment(){
 }
-$(".background").html("<img src=\"http://downloadwallpaperhd.com/wp-content/uploads/2013/04/The-Cube-3D-HD-Wallpaper.jpg" style=\"width: 921px; height: 285px; left: -218.5px;\">");
+$(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
 $('head').append('<link rel="stylesheet" type="text/css" href="http://www.gedankenwall.com/plugdj-chillroom-theme/2.css">');
