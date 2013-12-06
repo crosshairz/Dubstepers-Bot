02c7ebe7290536fbf91578c09792391c40762f52
Alexander7370 <alexanderkcato@gmail.com>
2013-12-05 19:48:41 -0800

    Create DB.js

---
+++ Bot/DB.js
@@ -0,0 +1,72 @@
+//this is the bot script for the new last plug API
+
+var mubBot = {};
+var ruleSkip = {};
+mubBot.misc = {};
+mubBot.settings = {};
+mubBot.moderators = {};
+mubBot.filters = {};
+botMethods = {};
+mubBot.pubVars = {};
+
+var BootMessage
+
+API.sendChat('/me : Running Dubstepers Script Version 2.0.1');
+
+getID = function(username){
+    var users = API.getUsers();
+    var result = "";
+    for(var i = 0; i < users.length; i++){
+        if(users[i].username === username){
+            result = users[i].id;
+            return result;
+        }
+    }
+
+    return "notFound";
+}; 
+
+
+
+//COMMANDZ
+API.on(API.CHAT, function(data){
+        if(data.message.indexOf('!') === 0){
+            var msg = data.message, from = data.from, fromID = data.fromID;
+            var command = msg.substring(1).split(' ');
+            if(typeof command[2] != "undefined"){
+                for(var i = 2; i<command.length; i++){
+                    command[1] = command[1] + ' ' + command[i];
+                }
+            }
+            
+            API.moderateDeleteChat(data.chatID);
+            
+            switch(command[0].toLowerCase() ){
+                    
+                case"ping":
+                        API.sendChat("@"+data.from+" PONG");
+                break//PING END
+                
+                case"info":
+                        API.sendChat("WORK IN PROGRESS COMMAND");
+                break//INFO END
+                
+                case"getid":
+                        API.sendChat(data.fromID); 
+                break//GETID END
+                
+                case"swapup":
+                        if(data.fromID == "5214f9e196fba51fda062ea7" || data.fromID == "524c910696fba55077fdaa12"){
+                                API.moderateMoveDJ(data.fromID, posWait = API.getWaitListPosition(data.fromID));
+                        };
+                break
+                
+                case"swapdown":
+                        if(data.fromID == "5214f9e196fba51fda062ea7" || data.fromID == "524c910696fba55077fdaa12")
+                                API.moderateMoveDJ(data.fromID, posWait = API.getWaitListPosition(data.fromID) + 2);
+                        };
+                break//
+        
+        }//End of Commands
+        }
+});//COMMAND END!!!!
