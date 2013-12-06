a2f0bc1682fa767fe47c7ba0976db8be4b25c372
Alexander7370 <alexanderkcato@gmail.com>
2013-12-05 19:41:27 -0800

    Create Custom_Theme.js

---
+++ Custom-Theme/Custom_Theme.js
@@ -0,0 +1,6 @@
+API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
+function initEnvironment(){
+}
+$(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
+$(background-image: url("http://downloadwallpaperhd.com/wp-content/uploads/2013/04/The-Cube-3D-HD-Wallpaper.jpg")!important; });
+
