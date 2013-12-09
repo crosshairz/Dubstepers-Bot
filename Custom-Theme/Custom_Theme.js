465a7dc75fa3bc726d1f88bf37c93d533b94198c
Alexander7370 <alexanderkcato@gmail.com>
2013-12-09 00:23:02 -0600

    Update Custom_Theme.js

--- Custom-Theme/Custom_Theme.js
+++ Custom-Theme/Custom_Theme.js
@@ -1,18 +1,5 @@
-//API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
-//function initEnvironment(){
-//}
-//$(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
-//$('head').append('<link rel="stylesheet" type="text/css" href="http://www.gedankenwall.com/plugdj-chillroom-theme/2.css">');
-
-
-
-
-
-
-
-
 API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
 function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
-$('head').append('<link rel="stylesheet" type="text/plain" href="https://raw.github.com/Alexander7370/Dubstepers-Bot/master/Custom-Theme/Custom_Theme_pic">');
+$('head').append('<link rel="stylesheet" type="text/css" href="http://www.gedankenwall.com/plugdj-chillroom-theme/2.css">');
