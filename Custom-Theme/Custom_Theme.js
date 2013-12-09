2e41d32f25c594452473f3d879af39760f373b17
Alexander7370 <alexanderkcato@gmail.com>
2013-12-09 07:30:36 -0600

    Update Custom_Theme.js

--- Custom-Theme/Custom_Theme.js
+++ Custom-Theme/Custom_Theme.js
@@ -2,4 +2,4 @@ API.chatLog("You are running a custom background now. Hopefully you like it! If 
 function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
-$(".background").html("<img src=\"http://image-upload.de/image/2rhlyf/86c3dcaa92.jpg\" style=\"width: 1550px; height: 900px; left: -510.5px;\">");
+$(".background").html("<img src=\"http://image-upload.de/image/2rhlyf/86c3dcaa92.jpg\" style=\"width: 1570px; height: 900px; left: -550.5px;\">");
