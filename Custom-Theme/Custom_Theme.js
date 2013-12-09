3e331e0489caa8ac321c6e52a6f9f5e003537c92
Alexander7370 <alexanderkcato@gmail.com>
2013-12-09 01:08:00 -0600

    Update Custom_Theme.js

--- Custom-Theme/Custom_Theme.js
+++ Custom-Theme/Custom_Theme.js
@@ -3,5 +3,5 @@ function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
 //$('head').append('<link rel="stylesheet" type="text/css" href="https://raw.github.com/Alexander7370/Dubstepers-Bot/master/Custom-Theme/Custom_Theme_pic.css">');
-$(".background").html("<img src=\"http://i.imgur.com/hb1kZvD.jpg\"style=\"width: 1600px; height: 1000px;\">");
+$(".background").html("<img src=\"http://i.imgur.com/hb1kZvD.jpg\"style=\"width: 1600px; height: 1000px;\"left: -518.5px\">");
 //("html { background: #000 url(http://i.imgur.com/hb1kZvD.jpg) no-repeat scroll center top; } #room-wheel { display: none !important; }")
