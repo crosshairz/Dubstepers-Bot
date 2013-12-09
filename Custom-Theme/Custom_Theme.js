0721109c8b78168364f07d02c150fcb5139406c1
Alexander7370 <alexanderkcato@gmail.com>
2013-12-09 00:59:52 -0600

    Update Custom_Theme.js

--- Custom-Theme/Custom_Theme.js
+++ Custom-Theme/Custom_Theme.js
@@ -3,4 +3,5 @@ function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
 //$('head').append('<link rel="stylesheet" type="text/css" href="https://raw.github.com/Alexander7370/Dubstepers-Bot/master/Custom-Theme/Custom_Theme_pic.css">');
-$(".background").html("<img src=\"http://4.bp.blogspot.com/-qFdXdZfkRPE/T74LfN4UxnI/AAAAAAAAEh4/4JxU8SIUXx8/s1600/cool-3d-background-1680x1050.jpg\"style=\"width: 1600px; height: 1000px; left: -218.5px;\">");
+//$(".background").html("<img src=\"http://4.bp.blogspot.com/-qFdXdZfkRPE/T74LfN4UxnI/AAAAAAAAEh4/4JxU8SIUXx8/s1600/cool-3d-background-1680x1050.jpg\"style=\"width: 1600px; height: 1000px; left: -218.5px;\">");
+GM_addStyle("html { background: #000 url(https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-ash3/1014702_619414634770059_130631835_o.jpg) no-repeat scroll center top; } #room-wheel { display: none !important; }")
