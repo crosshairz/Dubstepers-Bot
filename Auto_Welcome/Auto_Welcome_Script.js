c607bafe53a5abe668c665c40bba939470854126
foxdemon143 <foxdemon143@gmail.com>
2014-01-06 15:47:06 -0600

    Update Auto_Welcome_Script.js

--- Auto_Welcome/Auto_Welcome_Script.js
+++ Auto_Welcome/Auto_Welcome_Script.js
@@ -4,7 +4,7 @@ message = prompt("Message here the username will go at the end");
 
 API.on(API.USER_JOIN, function(data){
 
-if(message == null){
+if(message == ""){
 API.sendChat("Welcome to Dubstepers @" + data.username);
 } else {
 API.sendChat(message + " @" + data.username);
