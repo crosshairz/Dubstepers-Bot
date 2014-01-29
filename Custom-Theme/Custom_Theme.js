60b46dcc96aec66a157271ad6ca2885ecae04b21
Alexander7370 <alexanderkcato@gmail.com>
2014-01-29 16:52:47 -0600

    Update Custom_Theme.js

--- Custom-Theme/Custom_Theme.js
+++ Custom-Theme/Custom_Theme.js
@@ -1,7 +1,7 @@
 API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload. (some code by fungus)", true);
 
 $(".background").html("<img src=\"http://i.imgur.com/CflADad.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
-$('head').append('<link rel="stylesheet" type="text/css" href="http://www.gedankenwall.com/plugdj-chillroom-theme/2.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="https://raw.github.com/Alexander7370/Dubstepers-Bot/master/Custom-Theme/Custom_Theme_pic.css">');
 
 //Script that shows a tooltip with the full song name when you hover over the song name at the top of the screen.
 
