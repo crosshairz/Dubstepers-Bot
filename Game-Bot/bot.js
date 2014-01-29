310069139ee762b9d3cdfb388b658254b3c1201f
foxdemon143 <foxdemon143@gmail.com>
2014-01-29 17:01:11 -0600

    Update bot.js

--- Game-Bot/bot.js
+++ Game-Bot/bot.js
@@ -6,7 +6,7 @@ Version = "1.0.1";
 API.sendChat("Running Derpsteper's-GameBot Script Version " + Version);
 
 API.on(API.CHAT, function(data){
-if(data.message.indexOf('G!') === 0){
+if(data.message.indexOf('-') === 0){
             var msg = data.message, from = data.from, fromID = data.fromID;
             var command = msg.substring(1).split(' ');
             if(typeof command[2] != "undefined"){
