e115f344ddb406ea7a68d2c6af1409bba65ff1c0
Alexander7370 <alexanderkcato@gmail.com>
2014-02-02 14:05:08 -0600

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -17,7 +17,7 @@ mubBot.misc.Rules = "1.Do not play troll songs 2.Do not ask for ranks 3.Follow t
 mubBot.misc.Commands = "go to https://docs.google.com/document/d/1azqfktew9IJFP4gaXWbS9aLuKyyjPMOMeFNGxxeRCKY/edit?pli=1"
 mubBot.misc.Themes = "Dubstep, Remix's, and techno.   No Deathstep!";
 mubBot.misc.help = "Type !rules for rules and !commands for commands" //What Help will say
-mubBot.misc.version = "1.1.0";
+mubBot.misc.version = "1.1.1";
 mubBot.misc.origin = "This bot was created by Dubstepers, Alexander7370, and Foxdemon143. This bot and its script are copyrighted!";
 mubBot.misc.changelog = "Bot now deletes typed command after processing";
 mubBot.misc.ready = true;
