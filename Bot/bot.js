c4e0bc82c7a8eb3de09afc2e3bd71cd15ed65af8
Alexander7370 <alexanderkcato@gmail.com>
2014-02-16 15:38:54 -0600

    added auto fan!!!!

--- Bot/bot.js
+++ Bot/bot.js
@@ -1056,5 +1056,26 @@ botMethods.djAdvanceEvent = function(data){
             client_id: 'eae62c8e7a30564e9831b9e43f1d484a'
         });
     }, 3000);
+    
+    if (localStorage.usData === undefined) {
+    localStorage.usData = JSON.stringify({
+        counter: 0,
+    })
+}
+ 
+ 
+ 
+function fanEveryone(data) {
+    var relationship = require('app/models/TheUserModel');
+    if (relationship.getRelationship(data.id) < 2) {
+        var fan = require('app/services/user/UserFanService');
+        fan = new fan(true, data.id);
+          var totalCount = JSON.parse(localStorage.usData);
+        ++totalCount.counter;
+        console.log('Fanned new user: ' + data.username + '. Total number fanned: ' + totalCount.counter);
+        localStorage.usData = JSON.stringify(totalCount);
+    }
+}
+API.on(API.USER_JOIN, fanEveryone);
 
     API.sendChat('/me : Running Dubstepers Script '+mubBot.misc.version)
