6e22bc14a18dadad838e34ebee74a11b1b1b4643
foxdemon143 <foxdemon143@gmail.com>
2014-06-19 19:18:26 -0500

    Added Message Relize

--- Bot/bot.js
+++ Bot/bot.js
@@ -182,7 +182,7 @@ botMethods.djAdvanceEvent = function(data){
                 }
             }
             API.moderateDeleteChat(data.chatID);
-            console.log(data.from + '');
+            console.log(data.from + ' : ' + data.message);
             if(mubBot.misc.ready || mubBot.admins.indexOf(fromID) > -1 || API.getUser(data.fromID).permission > 1){
                 switch(command[0].toLowerCase()){
                     case "ping":
