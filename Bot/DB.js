7726e7ce3d194faae6d7e9967d59667f030d229a
foxdemon143 <foxdemon143@gmail.com>
2014-12-01 20:10:13 -0600

    Bug Fixes

--- Bot/DB.js
+++ Bot/DB.js
@@ -75,37 +75,34 @@ API.on(API.CHAT, function(data){
                     command[1] = command[1] + ' ' + command[i];
                 }
             }
-            
-            API.moderateDeleteChat(data.chatID);
-            
-            switch(command[0].toLowerCase() ){
+			switch(command[0].toLowerCase()){
                     
-                case"ping":
+                case "ping":
                         API.sendChat("@"+data.from+" PONG");
                 break;//PING END
                 
-                case"info":
+                case "info":
                         API.sendChat("WORK IN PROGRESS COMMAND");
                 break;//INFO END
                 
-                case"getid":
+                case "getid":
                         API.sendChat(data.fromID); 
                 break;//GETID END
-                
-                case"swapup":
+                /*
+                case "swapup":
                         if(data.fromID == "5214f9e196fba51fda062ea7" || data.fromID == "524c910696fba55077fdaa12"){
                                 API.moderateMoveDJ(data.fromID, posWait = API.getWaitListPosition(data.fromID));
                         };
                 break;
                 
-                case"swapdown":
+                case "swapdown":
                         if(data.fromID == "5214f9e196fba51fda062ea7" || data.fromID == "524c910696fba55077fdaa12")
                                 API.moderateMoveDJ(data.fromID, posWait = API.getWaitListPosition(data.fromID) + 2);
                         };
-                break;//
-        
+                break; */
+                
         }//End of Commands
-});//COMMAND END!!!!
+}});//COMMAND END!!!!
 
 
 
