306a554a6397f531becd7dab5bd095a25707466e
foxdemon143 <foxdemon143@gmail.com>
2014-12-01 20:07:42 -0600

    Update DB.js

--- Bot/DB.js
+++ Bot/DB.js
@@ -16,38 +16,38 @@ version = " 0.1 Beta";
 Startup = function(versionrun){
 
     Randomize = Math.floor((Math.random() * 10) + 1);
-	vermessage = ("    -Running Version: " + versionrun);
+	vermessage = (" : Running Version: " + versionrun);
 	
 		switch(Randomize){
 			case 1:
-				API.sendChat('/me Dubsteper\'s bot is now at your service---' + vermessage);
+				API.sendChat('/me Dubsteper\'s bot is now at your service' + vermessage);
 				break;
 			case 2:
-				API.sendChat('/me I\'m up and running! ---' + vermessage);
+				API.sendChat('/me I\'m up and running!' + vermessage);
 				break;
 			case 3:
-				API.sendChat('/me I may not be Jarvis, but I sure am one hell of an AI! ---' + vermessage);
+				API.sendChat('/me I may not be Jarvis, but I sure am one hell of an AI!' + vermessage);
 				break;
 			case 4:
-				API.sendChat('/me Bot is now online! ---' + vermessage);
+				API.sendChat('/me Bot is now online!' + vermessage);
 				break;
 			case 5:
-				API.sendChat('/me I am simply one hell of a bot! ---' + vermessage);
+				API.sendChat('/me I am simply one hell of a bot!' + vermessage);
 				break;
 			case 6:
-				API.sendChat('/me  6');
+				API.sendChat('/me  6' + vermessage);
 				break;
 			case 7:
-				API.sendChat('/me 7');
+				API.sendChat('/me 7' + vermessage);
 				break;
 			case 8:
-				API.sendChat('/me 8');
+				API.sendChat('/me 8' + vermessage);
 				break;
 			case 9:
-				API.sendChat('/me 9');
+				API.sendChat('/me 9' + vermessage);
 				break;
 			default:
-				API.sendChat('/me Dubsteper\'s room bot at your service! ---' = varmessage)
+				API.sendChat('/me Dubsteper\'s room bot at your service!' + vermessage);
 				break;
 		}
 };
