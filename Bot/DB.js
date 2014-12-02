4d8e8b8cb8cb10eaa40c162d16cdbc2bb74cd4ae
foxdemon143 <foxdemon143@gmail.com>
2014-12-01 20:35:05 -0600

    Update DB.js

--- Bot/DB.js
+++ Bot/DB.js
@@ -92,7 +92,9 @@ API.on(API.CHAT, function(data){
                 break; //SWAPDOWN END
                 
         }//End of Commands
-);//COMMAND END!!!!
+        
+        
+});//COMMAND FUNCTION END!!!!
 
 
 
