7802a8375d3ae71d30b4722c815d21a501c182bd
foxdemon143 <foxdemon143@gmail.com>
2014-12-01 20:48:03 -0600

    Update DB.js

--- Bot/DB.js
+++ Bot/DB.js
@@ -65,7 +65,7 @@ API.on(API.CHAT, function(data){
                     command[1] = command[1] + ' ' + command[i];
                 }
             }
-            //API.moderateDeleteChat(data.uid);
+            API.moderateDeleteChat(data.cid);
 			switch(command[0].toLowerCase()){
                     
                 case "ping":
